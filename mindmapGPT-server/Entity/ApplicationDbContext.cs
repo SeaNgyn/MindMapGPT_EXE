ac26ea9a94cc0d740f969fded8395651b2fa05c0
SeaNgyn <nguyentheanh2k3@gmail.com>
2025-03-05 09:05:42 +0700

    Initial Commit BackEnd

---
+++ mindmapGPT-server/Entity/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mindmapGPT_server.Entity
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Đảm bảo Email là duy nhất (nếu cần)
+            modelBuilder.Entity<UserEntity>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+    }
+}
