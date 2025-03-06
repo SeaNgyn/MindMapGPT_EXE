015f6a29fb4742f2f32a554918cc3e492271f84c
SeaNgyn <nguyentheanh2k3@gmail.com>
2025-03-06 18:10:58 +0700

    Fix cors

--- mindmapGPT-server/Program.cs
+++ mindmapGPT-server/Program.cs
@@ -41,7 +41,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-app.UseCors("AllowOrigin");
+app.UseCors("AllowAll");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
