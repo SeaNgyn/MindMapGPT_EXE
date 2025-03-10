aa963390d912efe7fe73d42e6c7be7074457e847
SeaNgyn <nguyentheanh2k3@gmail.com>
2025-03-10 19:44:44 +0700

    fix cors

--- mindmapGPT-server/Program.cs
+++ mindmapGPT-server/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", builder =>
     {
-        builder.WithOrigins("http://mindmapgpt.space") // Thay thế bằng domain của frontend
+        builder.WithOrigins("https://mindmapgpt.space") // Thay thế bằng domain của frontend
                .AllowAnyMethod()
                .AllowAnyHeader()
                .AllowCredentials(); // Quan trọng: Cho phép gửi credentials
